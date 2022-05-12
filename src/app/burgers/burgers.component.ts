6847b501ca317d9cfa29f50bd13d101732f19141
shawnnolanjr <shawn@shawnnolan.com>
2022-05-12 14:35:51 -0700

    - Adding menu and burgers stuff...

---
+++ src/app/burgers/burgers.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core'
+
+@Component({
+	selector: 'app-burgers',
+	templateUrl: './burgers.component.html',
+	styleUrls: ['./burgers.component.scss']
+})
+export class BurgersComponent {
+
+	constructor () {}
+
+}
--- src/app/explore-container/explore-container.component.ts
+++ src/app/explore-container/explore-container.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core'
 
 @Component({
-  selector: 'app-explore-container',
-  templateUrl: './explore-container.component.html',
-  styleUrls: ['./explore-container.component.scss'],
+	selector: 'app-explore-container',
+	templateUrl: './explore-container.component.html',
+	styleUrls: ['./explore-container.component.scss']
 })
 export class ExploreContainerComponent implements OnInit {
-  @Input() name: string;
+	@Input() name: string
+	@ViewChild('contentWrapper') content: ElementRef
 
-  constructor() { }
+	constructor () { }
 
-  ngOnInit() {}
+	ngOnInit () {}
 
 }
