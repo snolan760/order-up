79714f7b75aaccdffaea1504b68451768c7d9083
shawnnolanjr <shawn@shawnnolan.com>
2022-09-16 22:27:17 -0700

    - formatting

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,6 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
 import { TestBed, waitForAsync } from '@angular/core/testing'
-
 import { RouterTestingModule } from '@angular/router/testing'
-
 import { AppComponent } from './app.component'
 
 describe( 'AppComponent', () => {
